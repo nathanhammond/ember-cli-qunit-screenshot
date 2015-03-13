7c49f001bf0996d9951e986a926400a5711d25ab
Nathan Hammond <github.com@nathanhammond.com>
2015-03-13 02:58:41 -0400

    Must return a promise.

--- blueprints/ember-cli-qunit-screenshot/index.js
+++ blueprints/ember-cli-qunit-screenshot/index.js
@@ -6,14 +6,12 @@ module.exports = {
   },
 
   afterInstall: function() {
-    var npm = this.addPackagesToProject([
+    return this.addPackagesToProject([
       { name: 'screenshot-server', target: '0.0.x' }
-    ]);
-
-    var bower = this.addBowerPackagesToProject([
-      { name: 'screenshot-client', target: '0.0.x' }
-    ]);
-
-    return npm && bower;
+    ]).then(function() {
+      this.addBowerPackagesToProject([
+        { name: 'screenshot-client', target: '0.0.x' }
+      ]);
+    }.bind(this));
   }
 };
